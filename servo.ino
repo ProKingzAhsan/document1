d9ba1dd69fc2c23a26f78f97e6900d5f04d49742
Altamis Hail Ahsan NST <70384945+ProKingzAhsan@users.noreply.github.com>
2020-08-29 06:56:39 +0700

    Add files via upload

---
+++ servo.ino
@@ -0,0 +1,26 @@
+
+#include <Servo.h>
+
+Servo myservo;
+int pos = 0;
+
+void setup() {
+  myservo.attach(16);
+  pinMode (5, OUTPUT);
+}
+
+void loop(){
+  for(pos = 0; pos < 180; pos += 1){
+    myservo.write(pos);
+    delay(10);
+  }
+  for (pos = 180; pos>=1; pos -= 1){
+    myservo.write(pos);
+    delay(10);                                                                                                                                                                                                                                       
+  }
+
+  digitalWrite(5, HIGH);
+  delay(1000);
+  digitalWrite(5, LOW);
+  delay(1000);
+}
